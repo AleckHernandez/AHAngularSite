8c7751ce50bf8a8ed6c994de2bdbf06c152cbc41
73125960+AleckHernandez@users.noreply.github.com <73125960+AleckHernandez@users.noreply.github.com>
2023-12-02 20:37:11 -0500

    IN PROGRESS header bar for mobile

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'portfolio';
+
+  public screenWidth: any;
+  public screenHeight: any;
+  public isMobileMenuOpen: boolean = false;
+
+  ngOnInit() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+
+  toggleMobileMenu(event: any) {
+    const menu = event.currentTarget;
+    menu.classList.toggle('open');
+
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+
+    const header = document.querySelector('header');
+    if (header) {
+      header.classList.toggle('expanded', this.isMobileMenuOpen);
+    }
+  }
 }
